9870b630e930eb88a0a1f113bf141aad640beee4
stevepae <stevepaemusic@gmail.com>
2021-05-26 22:49:48 -0400

    completed kit with key presses and mouse clicks

--- index.js
+++ index.js
@@ -1,38 +1,55 @@
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     let buttonInnerHTML = this.innerHTML;
-    switch (buttonInnerHTML) {
-      case "w":
-        let kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
-        break;
-      case "a":
-        let snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-      case "s":
-        let tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-      case "d":
-        let tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-      case "j":
-        let tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-      case "k":
-        let tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-      case "l":
-        let crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-
-      default:
-        console.log(button.innerHTML);
-    }
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
+}
+
+document.addEventListener("keydown", function (e) {
+  makeSound(e.key);
+  buttonAnimation(e.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      let kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "a":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "s":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "d":
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "j":
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "k":
+      let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "l":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    default:
+      console.log(button.innerHTML);
+  }
+}
+function buttonAnimation(currentKey) {
+  let activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
